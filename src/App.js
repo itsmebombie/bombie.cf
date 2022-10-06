7b83fa325f61a5d0cfea8a69251c64b0902be150
itsmebombie <92307212+itsmebombie@users.noreply.github.com>
2022-10-06 18:34:10 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div class="App">
+      <header class="App-header">
+        <img class="blck" width="500" height="500" src="blck.webp" alt="stop posting about blck im tired of seeing it my friends on tiktok " />
+        <iframe class="youtube" width="500" height="280" src="https://www.youtube.com/embed/ZMQX9Son3gY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+
+        <p class="text">stop asking where/how to download blck </p> 
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          class="link"
+          href="https://discord.com/channels/791323294301290546/791324033240530955/1014544806204149840"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          you should check this out btw
         </a>
       </header>
     </div>
