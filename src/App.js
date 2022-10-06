2f2c3330e1940c646261508992c12449fbfc61e4
itsmebombie <92307212+itsmebombie@users.noreply.github.com>
2022-10-06 12:01:54 +0300

    fix indentantion

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img src="../public/blck.png" alt="stop posting about blck im tired of seeing it my friends on tiktok " width="55%" height="55%" />
+        <img src="../public/blck.png" alt="stop posting about blck im tired of seeing it my friends on tiktok " width="55%" height="55%" />
         <iframe width="55%" height="30%" 
           src="https://www.youtube.com/embed/ZMQX9Son3gY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
         </iframe>
