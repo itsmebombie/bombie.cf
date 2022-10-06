f8dbdaa7481de11e4b67fb39bd008b876d3c4985
itsmebombie <92307212+itsmebombie@users.noreply.github.com>
2022-10-06 10:55:04 +0300

    mobile coding 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="blck.png" alt="stop posting about blck im tired of seeing it my friends on tiktok " width="696" height="696" />
-        <iframe width="560" height="315" 
+        <iframe width="55%" height="20%" 
           src="https://www.youtube.com/embed/ZMQX9Son3gY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
         </iframe>
         
